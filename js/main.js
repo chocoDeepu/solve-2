d10fe9f1b8f100a65e1cc953c870e620ff7bfe8f
chocDeepu <aideepu7@gmail.com>
2020-07-09 17:43:12 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#sidenav-collapse').click(function(){
+        $('#mbl-collapse').slideToggle(1000);
+    });
+});
+$(document).ready(function() {
+	$('.gallery-image').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
+});
